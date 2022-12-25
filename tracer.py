e9c65f9c8182282cf0706b3ae4fe91117ea1b0f4
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-12-25 20:01:08 +0530

    Add files via upload

---
+++ tracer.py
@@ -0,0 +1,10 @@
+import geocoder
+import folium
+
+g = geocoder.ip("me")
+myAddress = g.latlng
+print(myAddress)
+myMap = folium.Map(location=myAddress, zoom_start=12)
+folium.CircleMarker(location=myAddress, radius=50, popup="You").add_to(myMap)
+folium.Marker(myAddress, popup="You").add_to(myMap)
+myMap.save("mymap.html")
